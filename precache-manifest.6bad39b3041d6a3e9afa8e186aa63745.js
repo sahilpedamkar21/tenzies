0d6ce4e78c53e421ce000eaee961c6e6f9dadb9d
sahilpedamkar21 <sahilpedamkar18@gmail.com>
2022-01-15 21:30:35 +0530

    Updates

---
+++ precache-manifest.6bad39b3041d6a3e9afa8e186aa63745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1e1c0b28143315b399935b015d6f5c",
+    "url": "/tenzies/index.html"
+  },
+  {
+    "revision": "fdfa979c486e87208d17",
+    "url": "/tenzies/static/css/main.235130f6.chunk.css"
+  },
+  {
+    "revision": "f478d1c8c3169ac08a95",
+    "url": "/tenzies/static/js/2.a61901e5.chunk.js"
+  },
+  {
+    "revision": "fdfa979c486e87208d17",
+    "url": "/tenzies/static/js/main.4807e14e.chunk.js"
+  },
+  {
+    "revision": "7a31227686114fd466ee",
+    "url": "/tenzies/static/js/runtime~main.30ca8c69.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tenzies/precache-manifest.6bad39b3041d6a3e9afa8e186aa63745.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tenzies/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
